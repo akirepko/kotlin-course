bed40e70611b817dc2bd1be3d1119679992af35d
akirepko <akirepko@gmail.com>
2024-09-14 08:47:25 +0300

    четвертое занятие и домашка

---
+++ src/main/kotlin/org/akirepko/kotlincourse/lesson4/lesson4.kt
@@ -0,0 +1,16 @@
+package org.example.org.akirepko.kotlincourse.lesson_4
+
+class lesson4 {
+    val myFloat=Float.MAX_VALUE
+    val myInt=Int.MAX_VALUE
+    val myLong=Long.MAX_VALUE
+    val myByte=Byte.MAX_VALUE
+    val myDouble=Double.MAX_VALUE
+    val myShort=Short.MAX_VALUE
+
+    val isKotlinFun=true
+    val letter='s'
+
+    val numbers: Array<Int> = arrayOf(1,2,3,)
+    val strings: MutableList<String> = mutableListOf("ss","s")
+}
