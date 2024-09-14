3f66acf7f296f04a99fac18c686aebadc392a12f
akirepko <akirepko@gmail.com>
2024-09-14 15:26:51 +0300

    сделал как котлина файл

--- src/main/kotlin/org/akirepko/kotlincourse/lesson4/Homework4.kt
+++ src/main/kotlin/org/akirepko/kotlincourse/lesson4/Homework4.kt
@@ -3,43 +3,43 @@ package org.example.org.akirepko.kotlincourse.lesson4
 
 
 
-42 // Int
-98765432123456789L //Long
-23.45f //Float
-0.123456789 //Double
-"Kotlin & Java" // String
-false //Boolean
-'c' //Char
-500 // Int
-4294967296L //Long
-18.0f //Float
--0.001 //Double
-"OpenAI" // String
-“true” // String
-'9' //Char
-2048 // Int
-10000000000L //Long
-5.75f //Float
-1.414 //Double
-"Artificial Intelligence"// String
-false //Boolean
-'@' //Char
-1024 // Int
-1234567890123L //Long
-10.01f //Float
--273.15 //Double
-"SpaceX" // String
-true//Boolean
-“🤯” //Char
-‘65535’ // Int
-72057594037927935L //Long
-2.71828f //Float
-101.0101 //Double
-"Quantum Computing" // String
-false //Boolean
-'x' //Char
-314// Int
-123456789123456789L //Long
-6.626f //Float
-0.007 //Double
-"Android Studio"// String
+val num1:Int= 42 // Int
+val numL:Long=98765432123456789L //Long
+val numf:Float=23.45f //Float
+val numD:Double=0.123456789 //Double
+val myString7:String="Kotlin & Java" // String
+val myBoolean: Boolean=false //Boolean
+val mychar: Char='c' //Char
+val num2:Int= 500 // Int
+val numL1:Long=4294967296L //Long
+val numf1:Float=18.0f //Float
+val numD1:Double=-0.001 //Double
+val myString:String="OpenAI" // String
+//“true” - тут нужно поментяь кавычки тогда строка или убрать тогда булево
+val mychar4: Char='9' //Char
+val num3:Int=2048 // Int
+val numL2:Long=10000000000L //Long
+val numf2:Float=5.75f //Float
+val numD2:Double=1.414 //Double
+val myString1:String="Artificial Intelligence"// String
+val myBoolean1: Boolean=false //Boolean
+val mychar2: Char='@' //Char
+val num5:Int=1024 // Int
+val numL3:Long=1234567890123L //Long
+val numf4:Float=10.01f //Float
+val numD3:Double=-273.15 //Double
+val myString2:String="SpaceX" // String
+val myBoolean2: Boolean=true//Boolean
+// “🤯” //Char но не те кавычки
+//‘65535’ // Int  если убрать кавычки или string  если поменять кавычки
+val numL4:Long=72057594037927935L //Long
+val numf5:Float=2.71828f //Float
+val numD4:Double=101.0101 //Double
+val myString3:String="Quantum Computing" // String
+val myBoolean3: Boolean=false //Boolean
+val mychar1: Char= 'x' //Char
+val num6:Int=314// Int
+val numL5:Long=123456789123456789L //Long
+val numf6:Float=66.626f //Float
+val numD5:Double=0.007 //Double
+val myString4:String="Android Studio"// String
